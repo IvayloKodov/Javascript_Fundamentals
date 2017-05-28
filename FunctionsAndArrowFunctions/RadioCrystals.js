ae3bfd577f3a82d28888056de3bed31880b011a4
IvayloKodov <Ivailo_Kodov@abv.bg>
2017-05-28 20:13:55 +0300

    .

--- FunctionsAndArrowFunctions/RadioCrystals.js
+++ FunctionsAndArrowFunctions/RadioCrystals.js
@@ -1,5 +1,25 @@
-function radioCrystals() {
+//Not finished
 
+function radioCrystals([finalTickness, tickness]) {
+    let cut = (a) => a / 4;
+    let lap = (a) => a * 0.8;
+    let grind = (a) => a - 20;
+    let etch = (a) => a - 2;
+
+    //Called once
+    let xray = (a) => a + 1;
+
+    //Called after eaach operation
+    let transportAndWashing = (a) => Math.floor(a);
+
+    let operations = [cut, lap, grind, etch];
+
+    console.log(`Processing chunk ${tickness} microns`);
+
+    //Logic here
+
+
+    console.log(`Finished crystal ${finalTickness} microns`);
 }
 
 radioCrystals([1375, 50000]);
